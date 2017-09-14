d9e84317c38a477912360321488ec607dc0fdd86
KatalinaNadasi <katalina.nadasi@gmail.com>
2017-09-14 17:42:55 +0200

    style des bulles et anim

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,35 +6,23 @@ import {
 } from '../components/Header.style';
 import WhenInView from '../js/WhenInView';
 
-// const circle = document.getElementById('circle');
-addEventListener('scroll', function(){
-  console.log(`
-    taillePage: ${document.body.scrollHeight}
-    tailleAffichage: ${innerHeight}
-    positionScroll: ${pageYOffset}
-    `)
-});
-
-
 
 export default class Header extends React.Component {
-  constructor(){
-    super();
-    this.state = {
-      onShow : false,
-      opacity: 0,
-    }
-  }
-
-
-  onShow(){
-        this.setState({
-          opacity: this.state.opacity === 0 ? 1 : 0
-        })
-
-    }
-
-
+  // constructor(){
+  //   super();
+  //   this.state = {
+  //     onShow : false,
+  //     opacity: 0,
+  //   }
+  // }
+  //
+  //
+  // onShow(){
+  //       this.setState({
+  //         opacity: this.state.opacity === 0 ? 1 : 0
+  //       })
+  //
+  //   }
 
 
   render(){
@@ -47,127 +35,122 @@ export default class Header extends React.Component {
             <CircleTitle>Na<Black>d</Black>asi</CircleTitle>
             <CircleSub>Front-End Developer</CircleSub>
           </CircleTop>
-
-
-
           <CircleBottom>
-            <div className="bubbles">
-                  <Bubble1
-                    style={{opacity: this.state.opacity}}
-                    onClick={this.onShow.bind(this)}>
-                    <svg height="200" width="200">
-                      <Ellipse pink cx="100" cy="100" rx="90" ry="90" />
-                    </svg>
-                    <BubbleP>Animations</BubbleP>
-                  </Bubble1>
-                  <WhenInView>
-                    {({isInView}) =>
-                    <Bubble2 hide={!isInView}>
-                      <svg height="160" width="160">
-                        <Ellipse black cx="80" cy="80" rx="70" ry="70" />
-                      </svg>
-                      <BubbleP>HTML5 / CSS3</BubbleP>
-                    </Bubble2>
-                  }
-                  </WhenInView>
-                  <Bubble3>
-                    <svg height="160" width="160">
-                      <Ellipse pink cx="80" cy="80" rx="70" ry="70" />
-                    </svg>
-                    <BubblePmin>Responsive</BubblePmin>
-                  </Bubble3>
-                  <Bubble4>
-                    <svg height="200" width="200">
-                      <Ellipse black cx="100" cy="100" rx="100" ry="100" />
-                    </svg>
-                    <BubbleP>React</BubbleP>
-                  </Bubble4>
-                  <Bubble5>
-                    <svg height="100" width="100">
-                      <Ellipse black cx="50" cy="50" rx="50" ry="50" />
-                    </svg>
-                    <BubbleP>SVG</BubbleP>
-                  </Bubble5>
-                  <Bubble6>
-                    <svg height="100" width="100">
-                      <Ellipse pink cx="50" cy="50" rx="50" ry="50" />
-                    </svg>
-                    <BubblePmin>Github</BubblePmin>
-                  </Bubble6>
-                  <Bubble7>
-                    <svg height="40" width="40">
-                      <Ellipse black cx="20" cy="20" rx="20" ry="20" />
-                    </svg>
-                  </Bubble7>
-                  <Bubble8>
-                    <svg height="40" width="40">
-                      <Ellipse black cx="20" cy="20" rx="20" ry="20" />
-                    </svg>
-                  </Bubble8>
-                  <Bubble9>
-                    <svg height="40" width="40">
-                      <Ellipse pink cx="20" cy="20" rx="20" ry="20" />
-                    </svg>
-                  </Bubble9>
-                  <Bubble10>
-                    <svg height="40" width="40">
-                      <Ellipse pink cx="20" cy="20" rx="20" ry="20" />
-                    </svg>
-                  </Bubble10>
-                  <Bubble11>
-                    <svg height="200" width="200">
-                      <Ellipse black cx="100" cy="100" rx="100" ry="100" />
-                    </svg>
-                    <BubbleP>Javascript / JQUERY</BubbleP>
-                  </Bubble11>
-                  <Bubble12>
-                    <svg height="20" width="20">
-                      <Ellipse pink cx="10" cy="10" rx="10" ry="10" />
-                    </svg>
-                  </Bubble12>
-                  <Bubble13>
-                    <svg height="20" width="20">
-                      <ellipse className="bubble-black" cx="10" cy="10" rx="10" ry="10" />
-                    </svg>
-                  </Bubble13>
-                  <Bubble14>
-                    <svg height="20" width="20">
-                      <Ellipse pink cx="10" cy="10" rx="10" ry="10" />
-                    </svg>
-                  </Bubble14>
-                  <Bubble15>
-                    <svg height="100" width="100">
-                      <Ellipse black cx="50" cy="50" rx="50" ry="50" />
-                    </svg>
-                    <BubblePmin>npm, Webpack</BubblePmin>
-                  </Bubble15>
-                  <Bubble16>
-                    <svg height="20" width="20">
-                      <Ellipse pink cx="10" cy="10" rx="10" ry="10" />
-                    </svg>
-                  </Bubble16>
-                  <Bubble17>
-                    <svg height="20" width="20">
-                      <Ellipse pink cx="10" cy="10" rx="10" ry="10" />
-                    </svg>
-                  </Bubble17>
-                  <Bubble18>
-                    <svg height="20" width="20">
-                      <Ellipse pink cx="10" cy="10" rx="10" ry="10" />
-                    </svg>
-                  </Bubble18>
-                  <Bubble19>
-                    <svg height="40" width="40">
-                      <Ellipse black cx="20" cy="20" rx="20" ry="20" />
-                    </svg>
-                  </Bubble19>
-                  <Bubble20>
-                    <svg height="160" width="160">
-                      <Ellipse pink cx="80" cy="80" rx="70" ry="70" />
-                    </svg>
-                    <BubblePmin>Bootstrap</BubblePmin>
-                  </Bubble20>
+            <WhenInView>
+              {({isInView}) =>
+              <div className="bubbles" hide={!isInView}>
+                <Bubble1>
+                  <svg height="200" width="200">
+                    <Ellipse pink cx="100" cy="100" rx="90" ry="90" />
+                  </svg>
+                  <BubbleP>Animations</BubbleP>
+                </Bubble1>
+                <Bubble2>
+                  <svg height="160" width="160">
+                    <Ellipse black cx="80" cy="80" rx="70" ry="70" />
+                  </svg>
+                  <BubbleP>HTML5 / CSS3</BubbleP>
+                </Bubble2>
+                <Bubble3>
+                  <svg height="160" width="160">
+                    <Ellipse pink cx="80" cy="80" rx="70" ry="70" />
+                  </svg>
+                  <BubblePmin>Responsive</BubblePmin>
+                </Bubble3>
+                <Bubble4>
+                  <svg height="200" width="200">
+                    <Ellipse black cx="100" cy="100" rx="100" ry="100" />
+                  </svg>
+                  <BubbleP>React</BubbleP>
+                </Bubble4>
+                <Bubble5>
+                  <svg height="100" width="100">
+                    <Ellipse black cx="50" cy="50" rx="50" ry="50" />
+                  </svg>
+                  <BubbleP>SVG</BubbleP>
+                </Bubble5>
+                <Bubble6>
+                  <svg height="100" width="100">
+                    <Ellipse pink cx="50" cy="50" rx="50" ry="50" />
+                  </svg>
+                  <BubblePmin>Github</BubblePmin>
+                </Bubble6>
+                <Bubble7>
+                  <svg height="40" width="40">
+                    <Ellipse black cx="20" cy="20" rx="20" ry="20" />
+                  </svg>
+                </Bubble7>
+                <Bubble8>
+                  <svg height="40" width="40">
+                    <Ellipse black cx="20" cy="20" rx="20" ry="20" />
+                  </svg>
+                </Bubble8>
+                <Bubble9>
+                  <svg height="40" width="40">
+                    <Ellipse pink cx="20" cy="20" rx="20" ry="20" />
+                  </svg>
+                </Bubble9>
+                <Bubble10>
+                  <svg height="40" width="40">
+                    <Ellipse pink cx="20" cy="20" rx="20" ry="20" />
+                  </svg>
+                </Bubble10>
+                <Bubble11>
+                  <svg height="200" width="200">
+                    <Ellipse black cx="100" cy="100" rx="100" ry="100" />
+                  </svg>
+                  <BubbleP>Javascript / JQUERY</BubbleP>
+                </Bubble11>
+                <Bubble12>
+                  <svg height="20" width="20">
+                    <Ellipse pink cx="10" cy="10" rx="10" ry="10" />
+                  </svg>
+                </Bubble12>
+                <Bubble13>
+                  <svg height="20" width="20">
+                    <ellipse className="bubble-black" cx="10" cy="10" rx="10" ry="10" />
+                  </svg>
+                </Bubble13>
+                <Bubble14>
+                  <svg height="20" width="20">
+                    <Ellipse pink cx="10" cy="10" rx="10" ry="10" />
+                  </svg>
+                </Bubble14>
+                <Bubble15>
+                  <svg height="100" width="100">
+                    <Ellipse black cx="50" cy="50" rx="50" ry="50" />
+                  </svg>
+                  <BubblePmin>npm, Webpack</BubblePmin>
+                </Bubble15>
+                <Bubble16>
+                  <svg height="20" width="20">
+                    <Ellipse pink cx="10" cy="10" rx="10" ry="10" />
+                  </svg>
+                </Bubble16>
+                <Bubble17>
+                  <svg height="20" width="20">
+                    <Ellipse pink cx="10" cy="10" rx="10" ry="10" />
+                  </svg>
+                </Bubble17>
+                <Bubble18>
+                  <svg height="20" width="20">
+                    <Ellipse pink cx="10" cy="10" rx="10" ry="10" />
+                  </svg>
+                </Bubble18>
+                <Bubble19>
+                  <svg height="40" width="40">
+                    <Ellipse black cx="20" cy="20" rx="20" ry="20" />
+                  </svg>
+                </Bubble19>
+                <Bubble20>
+                  <svg height="160" width="160">
+                    <Ellipse pink cx="80" cy="80" rx="70" ry="70" />
+                  </svg>
+                  <BubblePmin>Bootstrap</BubblePmin>
+                </Bubble20>
               </div>
+              }
+            </WhenInView>
           </CircleBottom>
         </Circle>
       </Wrapper>
--- src/components/Header.style.js
+++ src/components/Header.style.js
@@ -98,14 +98,46 @@ export const CircleTop = styled.div`
   position: absolute;
   left: 69%;
   top: 62%;
+	animation: ${jump} 25s linear, ${sideWays} 2s ease-in-out backwards;
+	display: block;
+
+	&:after {
+	 background: radial-gradient(ellipse at center,  rgba(255,255,255,0.5) 0%,rgba(255,255,255,0) 40%);
+	 filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#80ffffff', endColorstr='#00ffffff',GradientType=1 ); /* IE6-9 fallback on horizontal gradient */
+	 border-radius: 50%;
+	 box-shadow: inset 0 20px 30px rgba (255,255,255,0.3);
+	 content: "";
+	 height: 185px;
+	 left: 10px;
+	 position: absolute;
+	 width: 180px;
+	 top: 9px;
+	 }
+
+	${({hide}) => hide && css`
+		display: none;
+	`}
  `
  export const Bubble2 = styled.div`
   position: absolute;
   left: 24%;
   top: 69%;
 	animation: ${jump} 25s linear, ${sideWays} 2s ease-in-out backwards;
-	transition: 3s;
+	animation-delay: 0.2s;
 	display: block;
+
+	&:after {
+	 background: radial-gradient(ellipse at center,  rgba(255,255,255,0.5) 0%,rgba(255,255,255,0) 40%);
+	 filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#80ffffff', endColorstr='#00ffffff',GradientType=1 ); /* IE6-9 fallback on horizontal gradient */
+	 border-radius: 50%;
+	 box-shadow: inset 0 20px 30px rgba(255,255,255,0.3);
+	 content: "";
+	 height: 135px;
+	 left: 10px;
+	 position: absolute;
+ 	 width: 139px;
+	 top: 10px;
+	 }
 
 	${({hide}) => hide && css`
 		display: none;
@@ -116,112 +148,460 @@ export const CircleTop = styled.div`
    position: absolute;
    left: 36%;
    top: 82%;
+	 animation: ${jump} 25s linear, ${sideWays} 2s ease-in-out backwards;
+	 animation-delay: 0.4s;
+	 display: block;
 
 	 &:after {
-    background: radial-gradient(ellipse at center,  rgba(255,255,255,0.5) 0%,rgba(255,255,255,0) 70%); /* W3C */
+    background: radial-gradient(ellipse at center,  rgba(255,255,255,0.5) 0%,rgba(255,255,255,0) 40%);
     filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#80ffffff', endColorstr='#00ffffff',GradientType=1 ); /* IE6-9 fallback on horizontal gradient */
 		border-radius: 50%;
-		box-shadow: inset 0 20px 30px rgba(255, 255, 255, 0.3);
-		content: "";
-    height: 180px;
-		left: 10px;
-		position: absolute;
-		width: 180px;
+    box-shadow: inset 0 20px 30px rgba(255,255,255,0.3);
+    content: "";
+    height: 134px;
+    left: 10px;
+    position: absolute;
+    width: 139px;
+    top: 9px;
 		}
+
+		${({hide}) => hide && css`
+			display: none;
+		`}
  `
 
  export const Bubble4 = styled.div`
   position: absolute;
   left: 53%;
   top: 78%;
+	animation: ${jump} 25s linear, ${sideWays} 2s ease-in-out backwards;
+	animation-delay: 0.6s;
+	display: block;
+
+	&:after {
+	 background: radial-gradient(ellipse at center,  rgba(255,255,255,0.5) 0%,rgba(255,255,255,0) 40%);
+	 filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#80ffffff', endColorstr='#00ffffff',GradientType=1 ); /* IE6-9 fallback on horizontal gradient */
+	 border-radius: 50%;
+	 box-shadow: inset 0 20px 30px rgba (255,255,255,0.3);
+	 content: "";
+	 height: 185px;
+	 left: 10px;
+	 position: absolute;
+	 width: 180px;
+	 top: 9px;
+	 }
+
+	 ${({hide}) => hide && css`
+		 display: none;
+	 `}
  `
 
  export const Bubble5 = styled.div`
   position: absolute;
   left: 85%;
   top: 57%;
+	animation: ${jump} 25s linear, ${sideWays} 2s ease-in-out backwards;
+	animation-delay: 0.8s;
+	display: block;
+
+	&:after {
+	background: radial-gradient(ellipse at center,  rgba(255,255,255,0.5) 0%,rgba(255,255,255,0) 40%);
+	filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#80ffffff', endColorstr='#00ffffff',GradientType=1 ); /* IE6-9 fallback on horizontal gradient */
+	border-radius: 50%;
+  box-shadow: inset 0 20px 30px rgba(255,255,255,0.3);
+  content: "";
+  height: 100px;
+  left: 0px;
+  position: absolute;
+  width: 100px;
+  top: -0px;
+	}
+
+	 ${({hide}) => hide && css`
+		 display: none;
+	 `}
  `
  export const Bubble6 = styled.div`
   position: absolute;
   left: 39%;
   top: 62%;
+	animation: ${jump} 25s linear, ${sideWays} 2s ease-in-out backwards;
+	animation-delay: 1s;
+	display: block;
+
+	&:after {
+	 background: radial-gradient(ellipse at center,  rgba(255,255,255,0.5) 0%,rgba(255,255,255,0) 40%);
+	 filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#80ffffff', endColorstr='#00ffffff',GradientType=1 ); /* IE6-9 fallback on horizontal gradient */
+	 border-radius: 50%;
+	 box-shadow: inset 0 20px 30px rgba(255,255,255,0.3);
+	 content: "";
+	 height: 100px;
+	 left: 0px;
+	 position: absolute;
+	 width: 100px;
+	 top: 0px;
+	 }
+
+	 ${({hide}) => hide && css`
+		 display: none;
+	 `}
  `
 
  export const Bubble7 = styled.div`
   position: absolute;
   left: 35%;
   top: 55%;
+	animation: ${jump} 25s linear, ${sideWays} 2s ease-in-out backwards;
+	animation-delay: 1.2s;
+	display: block;
+
+	&:after {
+	background: radial-gradient(ellipse at center,  rgba(255,255,255,0.5) 0%,rgba(255,255,255,0) 40%);
+	filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#80ffffff', endColorstr='#00ffffff',GradientType=1 ); /* IE6-9 fallback on horizontal gradient */
+	border-radius: 50%;
+	box-shadow: inset 0 20px 30px rgba(255,255,255,0.3);
+	content: "";
+	height: 39px;
+  left: 0px;
+  width: 40px;
+  top: 0px;
+  position: absolute;
+	}
+
+	 ${({hide}) => hide && css`
+		 display: none;
+	 `}
  `
 
  export const Bubble8 = styled.div`
   position: absolute;
   left: 51%;
   top: 94%;
+	animation: ${jump} 25s linear, ${sideWays} 2s ease-in-out backwards;
+	animation-delay: 1.4s;
+	display: block;
+
+	&:after {
+	background: radial-gradient(ellipse at center,  rgba(255,255,255,0.5) 0%,rgba(255,255,255,0) 40%);
+	filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#80ffffff', endColorstr='#00ffffff',GradientType=1 ); /* IE6-9 fallback on horizontal gradient */
+	border-radius: 50%;
+	box-shadow: inset 0 20px 30px rgba(255,255,255,0.3);
+	content: "";
+	height: 39px;
+  left: 0px;
+  width: 40px;
+  top: 0px;
+  position: absolute;
+	}
+
+	 ${({hide}) => hide && css`
+		 display: none;
+	 `}
  `
 
  export const Bubble9 = styled.div`
   position: absolute;
   left: 67%;
   top: 69%;
+	animation: ${jump} 25s linear, ${sideWays} 2s ease-in-out backwards;
+	animation-delay: 1.6s;
+	display: block;
+
+	&:after {
+	background: radial-gradient(ellipse at center,  rgba(255,255,255,0.5) 0%,rgba(255,255,255,0) 40%);
+	filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#80ffffff', endColorstr='#00ffffff',GradientType=1 ); /* IE6-9 fallback on horizontal gradient */
+	border-radius: 50%;
+	box-shadow: inset 0 20px 30px rgba(255,255,255,0.3);
+	content: "";
+	height: 39px;
+  left: 0px;
+  width: 40px;
+  top: 0px;
+  position: absolute;
+	}
+
+	 ${({hide}) => hide && css`
+		 display: none;
+	 `}
  `
 
  export const Bubble10 = styled.div`
   position: absolute;
   left: 10%;
   top: 62%;
+	animation: ${jump} 25s linear, ${sideWays} 2s ease-in-out backwards;
+	animation-delay: 1.8s;
+	display: block;
+
+	&:after {
+	background: radial-gradient(ellipse at center,  rgba(255,255,255,0.5) 0%,rgba(255,255,255,0) 40%);
+	filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#80ffffff', endColorstr='#00ffffff',GradientType=1 ); /* IE6-9 fallback on horizontal gradient */
+	border-radius: 50%;
+	box-shadow: inset 0 20px 30px rgba(255,255,255,0.3);
+	content: "";
+	height: 39px;
+  left: 0px;
+  width: 40px;
+  top: 0px;
+  position: absolute;
+	}
+
+	 ${({hide}) => hide && css`
+		 display: none;
+	 `}
  `
 
  export const Bubble11 = styled.div`
   position: absolute;
   left: 50%;
   top: 56%;
+	animation: ${jump} 25s linear, ${sideWays} 2s ease-in-out backwards;
+	animation-delay: 2s;
+	display: block;
+
+	&:after {
+	 background: radial-gradient(ellipse at center,  rgba(255,255,255,0.5) 0%,rgba(255,255,255,0) 40%);
+	 filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#80ffffff', endColorstr='#00ffffff',GradientType=1 ); /* IE6-9 fallback on horizontal gradient */
+	 border-radius: 50%;
+	 box-shadow: inset 0 20px 30px rgba (255,255,255,0.3);
+	 content: "";
+	 height: 185px;
+	 left: 10px;
+	 position: absolute;
+	 width: 180px;
+	 top: 9px;
+	 }
+
+	 ${({hide}) => hide && css`
+		 display: none;
+	 `}
  `
 
  export const Bubble12 = styled.div`
   position: absolute;
   left: 92%;
   top: 64%;
+	animation: ${jump} 25s linear, ${sideWays} 2s ease-in-out backwards;
+	animation-delay: 2.2s;
+	display: block;
+
+	&:after {
+	 background: radial-gradient(ellipse at center,  rgba(255,255,255,0.5) 0%,rgba(255,255,255,0) 40%);
+	 filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#80ffffff', endColorstr='#00ffffff',GradientType=1 ); /* IE6-9 fallback on horizontal gradient */
+	 border-radius: 50%;
+	 box-shadow: inset 0 20px 30px rgba (255,255,255,0.3);
+	 content: "";
+	 height: 51px;
+	 left: -15px;
+	 position: absolute;
+	 width: 50px;
+	 top: -18px;
+	 }
+
+	 ${({hide}) => hide && css`
+		 display: none;
+	 `}
  `
 
  export const Bubble13 = styled.div`
   position: absolute;
   left: 75%;
   top: 86%;
+	animation: ${jump} 25s linear, ${sideWays} 2s ease-in-out backwards;
+	animation-delay: 2.4s;
+	display: block;
+
+	&:after {
+	 background: radial-gradient(ellipse at center,  rgba(255,255,255,0.5) 0%,rgba(255,255,255,0) 40%);
+	 filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#80ffffff', endColorstr='#00ffffff',GradientType=1 ); /* IE6-9 fallback on horizontal gradient */
+	 border-radius: 50%;
+	 box-shadow: inset 0 20px 30px rgba (255,255,255,0.3);
+	 content: "";
+	 height: 51px;
+	 left: -15px;
+	 position: absolute;
+	 width: 50px;
+	 top: -18px;
+	 }
+
+	 ${({hide}) => hide && css`
+		 display: none;
+	 `}
  `
 
  export const Bubble14 = styled.div`
   position: absolute;
   left: 69%;
   top: 53%;
+	animation: ${jump} 25s linear, ${sideWays} 2s ease-in-out backwards;
+	animation-delay: 2.6s;
+	display: block;
+
+	&:after {
+	 background: radial-gradient(ellipse at center,  rgba(255,255,255,0.5) 0%,rgba(255,255,255,0) 40%);
+	 filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#80ffffff', endColorstr='#00ffffff',GradientType=1 ); /* IE6-9 fallback on horizontal gradient */
+	 border-radius: 50%;
+	 box-shadow: inset 0 20px 30px rgba (255,255,255,0.3);
+	 content: "";
+	 height: 51px;
+	 left: -15px;
+	 position: absolute;
+	 width: 50px;
+	 top: -18px;
+	 }
+
+	 ${({hide}) => hide && css`
+		 display: none;
+	 `}
  `
 
  export const Bubble15 = styled.div`
   position: absolute;
   left: 24%;
   top: 83%;
+	animation: ${jump} 25s linear, ${sideWays} 2s ease-in-out backwards;
+	animation-delay: 2.8s;
+	display: block;
+
+	&:after {
+	 background: radial-gradient(ellipse at center,  rgba(255,255,255,0.5) 0%,rgba(255,255,255,0) 40%);
+	 filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#80ffffff', endColorstr='#00ffffff',GradientType=1 ); /* IE6-9 fallback on horizontal gradient */
+	 border-radius: 50%;
+	 box-shadow: inset 0 20px 30px rgba(255,255,255,0.3);
+	 content: "";
+	 height: 100px;
+	 left: 0px;
+	 position: absolute;
+	 width: 100px;
+	 top: 0px;
+	 }
+
+	 ${({hide}) => hide && css`
+		 display: none;
+	 `}
  `
  export const Bubble16 = styled.div`
   position: absolute;
   left: 13%;
   top: 75%;
+	animation: ${jump} 25s linear, ${sideWays} 2s ease-in-out backwards;
+	animation-delay: 3s;
+	display: block;
+
+	&:after {
+	 background: radial-gradient(ellipse at center,  rgba(255,255,255,0.5) 0%,rgba(255,255,255,0) 40%);
+	 filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#80ffffff', endColorstr='#00ffffff',GradientType=1 ); /* IE6-9 fallback on horizontal gradient */
+	 border-radius: 50%;
+	 box-shadow: inset 0 20px 30px rgba (255,255,255,0.3);
+	 content: "";
+	 height: 51px;
+	 left: -15px;
+	 position: absolute;
+	 width: 50px;
+	 top: -18px;
+	 }
+
+	 ${({hide}) => hide && css`
+		 display: none;
+	 `}
  `
  export const Bubble17 = styled.div`
   position: absolute;
   left: 20%;
   top: 57%;
+	animation: ${jump} 25s linear, ${sideWays} 2s ease-in-out backwards;
+	animation-delay: 3.2s;
+	display: block;
+
+	&:after {
+	 background: radial-gradient(ellipse at center,  rgba(255,255,255,0.5) 0%,rgba(255,255,255,0) 40%);
+	 filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#80ffffff', endColorstr='#00ffffff',GradientType=1 ); /* IE6-9 fallback on horizontal gradient */
+	 border-radius: 50%;
+	 box-shadow: inset 0 20px 30px rgba (255,255,255,0.3);
+	 content: "";
+	 height: 51px;
+	 left: -15px;
+	 position: absolute;
+	 width: 50px;
+	 top: -18px;
+	 }
+
+	 ${({hide}) => hide && css`
+		 display: none;
+	 `}
  `
  export const Bubble18 = styled.div`
   position: absolute;
   left: 45%;
   top: 78%;
+	animation: ${jump} 25s linear, ${sideWays} 2s ease-in-out backwards;
+	animation-delay: 3.4s;
+	display: block;
+
+	&:after {
+	 background: radial-gradient(ellipse at center,  rgba(255,255,255,0.5) 0%,rgba(255,255,255,0) 40%);
+	 filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#80ffffff', endColorstr='#00ffffff',GradientType=1 ); /* IE6-9 fallback on horizontal gradient */
+	 border-radius: 50%;
+	 box-shadow: inset 0 20px 30px rgba (255,255,255,0.3);
+	 content: "";
+	 height: 51px;
+	 left: -15px;
+	 position: absolute;
+	 width: 50px;
+	 top: -18px;
+	 }
+
+	 ${({hide}) => hide && css`
+		 display: none;
+	 `}
  `
  export const Bubble19 = styled.div`
   position: absolute;
   left: 14%;
   top: 55%;
+	animation: ${jump} 25s linear, ${sideWays} 2s ease-in-out backwards;
+	animation-delay: 3.6s;
+	display: block;
+
+	&:after {
+	background: radial-gradient(ellipse at center,  rgba(255,255,255,0.5) 0%,rgba(255,255,255,0) 40%);
+	filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#80ffffff', endColorstr='#00ffffff',GradientType=1 ); /* IE6-9 fallback on horizontal gradient */
+	border-radius: 50%;
+	box-shadow: inset 0 20px 30px rgba(255,255,255,0.3);
+	content: "";
+	height: 39px;
+  left: 0px;
+  width: 40px;
+  top: 0px;
+  position: absolute;
+	}
+
+	 ${({hide}) => hide && css`
+		 display: none;
+	 `}
  `
  export const Bubble20 = styled.div`
    position: absolute;
    left: 12%;
    top: 60%;
+	 animation: ${jump} 25s linear, ${sideWays} 2s ease-in-out backwards;
+	 animation-delay: 3.8s;
+	 display: block;
+
+	 &:after {
+    background: radial-gradient(ellipse at center,  rgba(255,255,255,0.5) 0%,rgba(255,255,255,0) 40%);
+    filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#80ffffff', endColorstr='#00ffffff',GradientType=1 ); /* IE6-9 fallback on horizontal gradient */
+		border-radius: 50%;
+    box-shadow: inset 0 20px 30px rgba(255,255,255,0.3);
+    content: "";
+    height: 134px;
+    left: 10px;
+    position: absolute;
+    width: 139px;
+    top: 9px;
+		}
+
+		${({hide}) => hide && css`
+			display: none;
+		`}
  `
