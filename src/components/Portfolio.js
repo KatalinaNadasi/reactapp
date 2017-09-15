3c715151f838948358d2f948ff0744b09d8cb8d3
KatalinaNadasi <katalina.nadasi@gmail.com>
2017-09-15 18:37:37 +0200

    repositionnement portfolio

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ParallaxImage from 'react-image-parallax2';
 import '../index.css';
-import {Container, IconTitle, Wrapper, ImageContainer1, ImageContainer2, Background1, Background2} from '../components/Portfolio.style';
+import {Container, IconTitle, Wrapper, ImageContainer1, ImageContainer2, ImageContainer3, ImageContainer4, ImageContainer5, ImageContainer6, Background1, Background2} from '../components/Portfolio.style';
 
 class Portfolio extends React.Component {
   render () {
@@ -21,31 +21,31 @@ class Portfolio extends React.Component {
               src={require('../img/portfolio/adaweek.jpg')}/>
           </ImageContainer2>
           <div className="clear"></div>
-            <ImageContainer1>
+            <ImageContainer3>
               <ParallaxImage
                 reduceHeight={2/3}
                 src={require('../img/portfolio/katalina.jpg')}/>
-            </ImageContainer1>
-            <ImageContainer2>
+            </ImageContainer3>
+            <ImageContainer4>
               <ParallaxImage
                 reduceHeight={0.2/3}
                 src={require('../img/portfolio/labanque.png')}/>
-            </ImageContainer2>
+            </ImageContainer4>
           <div className="clear"></div>
           </Wrapper>
         </Background1>
         <Background2>
           <Wrapper>
-            <ImageContainer1>
+            <ImageContainer5>
               <ParallaxImage
                 reduceHeight={2/3}
                 src={require('../img/portfolio/sneakr.jpg')}/>
-            </ImageContainer1>
-            <ImageContainer2>
+            </ImageContainer5>
+            <ImageContainer6>
               <ParallaxImage
                 reduceHeight={2/3}
                 src={require('../img/portfolio/eavest.jpg')}/>
-            </ImageContainer2>
+            </ImageContainer6>
             <div className="clear"></div>
           </Wrapper>
         </Background2>
--- src/components/Portfolio.style.js
+++ src/components/Portfolio.style.js
@@ -12,7 +12,7 @@ import {pink} from '../variables.js';
 // `
 
 export const Container = styled.div`
-  transform: translateY(-400px);
+  transform: translateY(-500px);
 `
 
 export const IconTitle = React.createClass({
@@ -23,7 +23,7 @@ export const IconTitle = React.createClass({
      	 xmlSpace="preserve">
        <text transform="matrix(0.9256 0 0 1 4 238.6028)" fontFamily="'NewsGothicMT-Bold'" fontSize="72">PRO</text>
        <text transform="matrix(0.9256 0 0 1 4 325.0032)" fontFamily="'NewsGothicMT-Bold'" fontSize="72">JECTS </text>
-      <ellipse fill="##FE4365" stroke="#FE4365" strokeMiterlimit="10" cx="239.675" cy="304.391" rx="23" ry="22.588"/>
+      <ellipse fill="#FE4365" stroke="#FE4365" strokeMiterlimit="10" cx="239.675" cy="304.391" rx="23" ry="22.588"/>
      </svg>
    )
  }
@@ -35,7 +35,7 @@ export const Wrapper = styled.div`
 `
 
 export const ImageContainer1 = styled.div`
-  width: 35%;
+  width: 60%;
   float: left;
   margin: 0 0 0 125px;
   transition: 0.5s all;
@@ -47,9 +47,53 @@ export const ImageContainer1 = styled.div`
 `
 
 export const ImageContainer2 = styled.div`
-  margin: 175px 0 0 125px;
-  width: 35%;
+  margin: -50px 0px 23px 125px;
+  width: 51%;
+  float: right;
+  transition: 0.5s all;
+  &:hover {
+    transform: scale(1.1, 1.1);
+    transition: 0.5s all;
+  }
+`
+
+export const ImageContainer3 = styled.div`
+  width: 50%;
   float: left;
+  margin: -345px 0 0 60px;
+  transition: 0.5s all;
+  &:hover {
+    transform: scale(1.1, 1.1);
+    transition: 0.5s all;
+  }
+`
+
+export const ImageContainer4 = styled.div`
+  width: 35%;
+  float: right;
+  margin: 25px 76px 25px 0;
+  transition: 0.5s all;
+  &:hover {
+    transform: scale(1.1, 1.1);
+    transition: 0.5s all;
+  }
+`
+
+export const ImageContainer5 = styled.div`
+  margin: 31px -52px 23px 0px;
+  float: right;
+  transition: 0.5s all;
+  width: 44%;
+  &:hover {
+    transform: scale(1.1, 1.1);
+    transition: 0.5s all;
+  }
+`
+
+export const ImageContainer6 = styled.div`
+  margin: -80px 0px 0px 71px;
+  width: 60%;
+  float: right;
   transition: 0.5s all;
   &:hover {
     transform: scale(1.1, 1.1);
