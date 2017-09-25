4cc9bf22456832a9afcbe5bf855ac6867c939aed
KatalinaNadasi <katalina.nadasi@gmail.com>
2017-09-25 12:22:53 +0200

    footer icons

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -12,7 +12,7 @@ const Wrapper = styled.div `
 `
 
 const Contact = styled.div `
-  width: 510px;
+  width: 900px;
   display: inline-block;
   vertical-align: middle;
   overflow: hidden;
@@ -21,7 +21,7 @@ const Contact = styled.div `
 
 const LeftCol = styled.ul `
   float: left;
-  width: 50%;
+  width: 300px;
   padding: 0 6px;
   list-style: none;
   margin: 0;
@@ -29,6 +29,7 @@ const LeftCol = styled.ul `
 
 const RightCol = LeftCol.extend `
   float: right;
+  width: 300px;
 `
 
 const Border = styled.li `
@@ -40,6 +41,43 @@ const Border = styled.li `
   }
 `
 
+const IconSocials = React.createClass({
+ render() {
+   return (
+     <svg version="1.1" id="socials" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+     	 width="300px" height="72px" viewBox="21.316 180.445 248.824 70" enableBackground="new 17.316 177.445 248.824 70"
+     	 xmlSpace="preserve">
+     <title  id="simpleicons-linkedin-icon">LinkedIn icon</title>
+     <ellipse id="work" fill="#F3F3ED" stroke="#E7E6D8" strokeMiterlimit="10" strokeWidth="3.04" cx="232.14" cy="213.445" rx="30" ry="30"/>
+       <text transform="matrix(1 0 0 1 210.9895 210.3413)">
+         <tspan x="-6" y="18" fontSize="41">C</tspan>
+         <tspan x="5" y="4" fontFamily="'OCRAStd'" fontSize="10">ontact</tspan>
+         <tspan x="17.132" y="11" fontFamily="'OCRAStd'" fontSize="10">me</tspan>
+       </text>
+     <ellipse id="linked" fill="#F3F3ED" stroke="#E7E6D8" strokeMiterlimit="10" strokeWidth="3.04" cx="51.316" cy="213.445" rx="30" ry="30"/>
+     <ellipse id="github" fill="#F3F3ED" stroke="#E7E6D8" strokeMiterlimit="10" strokeWidth="3.04" cx="141.679" cy="213.445" rx="30" ry="30"/>
+       <path d="M156.84,227.239h-6.378v-9.746c0-2.325-0.049-5.316-3.324-5.316c-3.327,0-3.834,2.529-3.834,5.145v9.918h-6.38v-20.043
+       	h6.128v2.731h0.082c0.856-1.575,2.938-3.238,6.05-3.238c6.463,0,7.658,4.147,7.658,9.548v11.002H156.84z M129.72,204.453
+       	c-2.053,0-3.703-1.62-3.703-3.615c0-1.991,1.65-3.609,3.703-3.609c2.045,0,3.704,1.618,3.704,3.609
+       	C133.424,202.833,131.764,204.453,129.72,204.453z M132.918,227.239h-6.397v-20.043h6.397V227.239z"/>
+       <g>
+     	  <title  id="simpleicons-github-icon">GitHub icon</title>
+     	  <path d="M51.315,186.175c-15.509,0-28.07,12.062-28.07,26.938c0,11.903,8.042,21.999,19.192,25.557
+     		c1.404,0.254,1.919-0.579,1.919-1.295c0-0.64-0.023-2.335-0.035-4.579c-7.809,1.625-9.455-3.614-9.455-3.614
+     		c-1.276-3.108-3.123-3.938-3.123-3.938c-2.543-1.67,0.196-1.638,0.196-1.638c2.819,0.188,4.3,2.774,4.3,2.774
+     		c2.503,4.119,6.57,2.93,8.175,2.24c0.253-1.741,0.977-2.93,1.778-3.604c-6.234-0.674-12.786-2.989-12.786-13.312
+     		c0-2.94,1.088-5.344,2.89-7.229c-0.315-0.68-1.264-3.418,0.245-7.129c0,0,2.352-0.723,7.72,2.761
+     		c2.246-0.599,4.632-0.896,7.018-0.908c2.386,0.014,4.772,0.311,7.019,0.908c5.333-3.482,7.685-2.761,7.685-2.761
+     		c1.509,3.711,0.561,6.449,0.28,7.129c1.79,1.886,2.877,4.288,2.877,7.229c0,10.349-6.562,12.627-12.808,13.289
+     		c0.982,0.809,1.896,2.46,1.896,4.982c0,3.605-0.035,6.501-0.035,7.376c0,0.707,0.491,1.549,1.93,1.28
+     		c11.23-3.533,19.267-13.635,19.267-25.521C79.385,198.235,66.816,186.175,51.315,186.175"/>
+       </g>
+     </svg>
+
+   )
+ }
+});
+
 export default class Footer extends Component {
   render(){
     return(
@@ -47,11 +85,14 @@ export default class Footer extends Component {
         <Contact>
           <LeftCol>
             <Border>katalina.nadasi@gmail.com</Border>
-            <Border>Work together</Border>
+            <Border>katalinanadasi.com</Border>
           </LeftCol>
           <RightCol>
             <Border>&copy; Katalina Nadasi 2017</Border>
             <Border>Design and Development</Border>
+          </RightCol>
+          <RightCol>
+            <IconSocials></IconSocials>
           </RightCol>
         </Contact>
 
