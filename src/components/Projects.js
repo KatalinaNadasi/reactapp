911c27242c0c25fcc26de21b899c9d4f7c9043a9
KatalinaNadasi <katalina.nadasi@gmail.com>
2017-10-10 09:10:36 +0200

    rajout photos carousel

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import Wrapper from './Wrapper';
-import {BackgroundBlue, Title, BackgroundImg} from './Projects.style';
+import {Background, Title} from './Projects.style';
 import { Carousel } from 'react-responsive-carousel';
 import '../style/carousel.css';
 
@@ -8,17 +8,27 @@ export default class Projects extends Component {
   render(){
     return(
       <Wrapper>
-        <BackgroundBlue>
+        <Background>
           <Title>Projects</Title>
-        </BackgroundBlue>
-        <Carousel>
-            <div>
-                <img src={require('../img/projects/chopard.jpg')} alt="chopard"/>
-            </div>
-            <div>
-                <img src={require('../img/projects/adaweek.png')} alt="adaweek" />
-            </div>
+        </Background>
+        <Carousel autoPlay interval={3000} transitionTime={1000} showThumbs={false} showStatus={false} showIndicators={false}>
+          <div>
+            <img height="650" src={require('../img/projects/chopard.jpg')} alt="chopard"/>
+          </div>
+          <div>
+            <img height="650" src={require('../img/projects/eavest.jpg')} alt="eavest" />
+          </div>
+          <div>
+            <img height="650" src={require('../img/projects/labanque.jpg')} alt="la banque postale" />
+          </div>
+          <div>
+            <img height="650" src={require('../img/projects/sneakr.jpg')} alt="sneakr" />
+          </div>
+          <div>
+            <img height="650" src={require('../img/projects/adaweek.jpg')} alt="adaweek" />
+          </div>
         </Carousel>
+        <div style={{height : 150}}></div>
       </Wrapper>
     );
   }
--- src/components/Projects.style.js
+++ src/components/Projects.style.js
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 import {darkcreme} from  '../variables';
 
 
-export const BackgroundBlue = styled.div `
+export const Background = styled.div `
   background-color: black;
   text-align: center;
   line-height: 563px;
@@ -16,25 +16,4 @@ export const Title = styled.h1 `
   background: -webkit-linear-gradient( left, #FFDC80 0%, #FCAF45 30%, #F56040 40%, #E71D36 50%, #280C7C 90%) repeat;
   -webkit-background-clip: text;
   -webkit-text-fill-color: transparent;
-`
-
-export const BackgroundImg = styled.div `
-  height: 79vh;
-  background-image: url('${require('../img/projects/adaweek.png')}');
-  background-repeat: no-repeat;
-  z-index: -99;
-  background-size: 100%;
-  width: 100%;
-  padding: -20px;
-  position: relative;
-  background-size: contain;
-  border: 3px #9CAAB6 solid;
-  background-color: white;
-}
-
-
-
-
-
-
 `
