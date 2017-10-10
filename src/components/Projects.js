cd49bee388dfb2d0914471d02deb5e43c4bf4f6c
KatalinaNadasi <katalina.nadasi@gmail.com>
2017-10-10 14:42:20 +0200

    rajout boutons carrousel

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import Wrapper from './Wrapper';
-import {Background, Title} from './Projects.style';
+import {Background, Title, Opacity, TextLeft, TextRight, Left, Right} from './Projects.style';
 import { Carousel } from 'react-responsive-carousel';
 import '../style/carousel.css';
 
@@ -11,23 +11,57 @@ export default class Projects extends Component {
         <Background>
           <Title>Projects</Title>
         </Background>
-        <Carousel autoPlay interval={3000} transitionTime={1000} showThumbs={false} showStatus={false} showIndicators={false}>
-          <div>
-            <img height="650" src={require('../img/projects/chopard.jpg')} alt="chopard"/>
-          </div>
-          <div>
-            <img height="650" src={require('../img/projects/eavest.jpg')} alt="eavest" />
-          </div>
-          <div>
-            <img height="650" src={require('../img/projects/labanque.jpg')} alt="la banque postale" />
-          </div>
-          <div>
-            <img height="650" src={require('../img/projects/sneakr.jpg')} alt="sneakr" />
-          </div>
-          <div>
-            <img height="650" src={require('../img/projects/adaweek.jpg')} alt="adaweek" />
-          </div>
-        </Carousel>
+        <Opacity>
+          <Carousel autoPlay interval={3000} transitionTime={1000} showThumbs={false} showStatus={false} showIndicators={false}>
+            <div>
+              <img height="650" src={require('../img/projects/chopard.jpg')} alt="Chopard"/>
+            </div>
+            <div>
+              <img height="650" src={require('../img/projects/eavest.jpg')} alt="Eavest" />
+            </div>
+            <div>
+              <img height="650" src={require('../img/projects/labanque.jpg')} alt="La Banque Postale" />
+            </div>
+            <div>
+              <img height="650" src={require('../img/projects/sneakr.jpg')} alt="Sneakr" />
+            </div>
+            <div>
+              <img height="650" src={require('../img/projects/adaweek.jpg')} alt="Adaweek" />
+            </div>
+            <div>
+              <img height="650" src={require('../img/projects/katalina.jpg')} alt="Katalina Nadasi" />
+            </div>
+            <div>
+              <img height="650" src={require('../img/projects/chopard.jpg')} alt="Chopard"/>
+            </div>
+          </Carousel>
+        </Opacity>
+        <Left>
+          <TextLeft>
+            <div>
+              <h2>Eavest</h2><p>Front-end developer</p>
+            </div>
+            <div>
+              <h2>Adaweek</h2><p>Front-end developer / Design</p>
+            </div>
+            <div>
+              <h2>Chopard</h2><p>Front-end developer</p>
+            </div>
+          </TextLeft>
+        </Left>
+        <Right>
+          <TextRight>
+            <div>
+              <h2>La Banque Postale</h2><p>Front-end developer</p>
+            </div>
+            <div>
+              <h2>Sneakr</h2><p>Front-end developer</p>
+            </div>
+            <div>
+              <h2>Personal Website</h2><p>Front-end developer</p>
+            </div>
+          </TextRight>
+        </Right>
         <div style={{height : 150}}></div>
       </Wrapper>
     );
--- src/components/Projects.style.js
+++ src/components/Projects.style.js
@@ -1,5 +1,5 @@
 import styled from 'styled-components';
-import {darkcreme} from  '../variables';
+import {darkcreme, lightpink} from  '../variables';
 
 
 export const Background = styled.div `
@@ -16,4 +16,50 @@ export const Title = styled.h1 `
   background: -webkit-linear-gradient( left, #FFDC80 0%, #FCAF45 30%, #F56040 40%, #E71D36 50%, #280C7C 90%) repeat;
   -webkit-background-clip: text;
   -webkit-text-fill-color: transparent;
+`
+
+export const Opacity = styled.div `
+  opacity : 0.6;
+`
+
+export const TextLeft = styled.div `
+  font-size: 3rem;
+  padding: 15px;
+  position: absolute;
+  bottom: 276px;
+  right: 550px;
+  color: black;
+  width: 30%;
+  float: left;
+
+  div {
+    padding: 35px;
+
+    &:hover {
+      background-color: #FF3E5F;
+      transition: all 0.5s ease-in;
+    }
+  }
+  h2 {
+    text-align: left;
+    font-size: 2rem;
+  }
+  p {
+    font-size: 1.3rem;
+    text-align: left;
+  }
+
+  }
+`
+
+export const TextRight = TextLeft.extend `
+  right: 148px;
+`
+
+export const Left = styled.div `
+  float: left;
+`
+
+export const Right = Left.extend `
+  float: right;
 `
